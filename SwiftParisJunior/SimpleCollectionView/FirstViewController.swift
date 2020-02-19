7faf3e49c84b0ea4a8e789172c78c5d31ad187b1
Paul <pbancarel@octo.com>
2020-02-19 17:02:03 +0100

    Simple collection view

--- SwiftParisJunior/SimpleCollectionView/FirstViewController.swift
+++ SwiftParisJunior/SimpleCollectionView/FirstViewController.swift
@@ -2,8 +2,32 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        collectionView.dataSource = self
+        collectionView.delegate = self
     }
 
 }
+
+extension FirstViewController : UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 100
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        
+        if indexPath.row == 0 {
+            cell.contentView.backgroundColor = .systemRed
+        } else {
+            cell.contentView.backgroundColor = .systemBlue
+        }
+        
+        return cell
+    }
+}
+
+extension FirstViewController : UICollectionViewDelegate {}
