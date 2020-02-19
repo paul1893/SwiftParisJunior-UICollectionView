e6bc0a8822fc82a480b738a2ee36057bd43b59a1
Paul <pbancarel@octo.com>
2020-02-19 17:31:59 +0100

    Second collection view with diffable datasource

--- SwiftParisJunior/CollectionViewDiffableDataSource/SecondViewController.swift
+++ SwiftParisJunior/CollectionViewDiffableDataSource/SecondViewController.swift
@@ -2,8 +2,22 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    private var dataSource: UICollectionViewDiffableDataSource<Int, Int>!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        dataSource = UICollectionViewDiffableDataSource(collectionView: collectionView, cellProvider: { (collectionView, indexPath, item) -> UICollectionViewCell? in
+            let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+            cell.contentView.backgroundColor = .systemBlue
+            return cell
+        })
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Int, Int>()
+        snapshot.appendSections([0, 1])
+        snapshot.appendItems([0, 1, 2, 3], toSection: 0)
+        snapshot.appendItems([4, 5, 6], toSection: 1)
+        dataSource.apply(snapshot, animatingDifferences: true)
     }
     
 }
