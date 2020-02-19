2697fab587131407221d312c89e1658ab1a70c13
Paul <pbancarel@octo.com>
2020-02-19 16:50:21 +0100

    Init structure

---
+++ SwiftParisJunior/CollectionViewDiffableDataSource/SecondViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
---
+++ SwiftParisJunior/CompositionalLayout/ThirdViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
